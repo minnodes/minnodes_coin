d29df22c43daf8d8911b9e6f38eec6834ed333d4
Phil <nicky@minnodes.io>
2019-03-18 02:28:33 -0700

    fix disconnect form peers

--- src/version.h
+++ src/version.h
@@ -20,11 +20,11 @@ static const int INIT_PROTO_VERSION = 209;
 static const int GETHEADERS_VERSION = 70077;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 809011;
+static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 809010;
 static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 809011;
 
 //! masternodes older than this proto version use old strMessage format for mnannounce
-static const int MIN_PEER_MNANNOUNCE = 809011;
+static const int MIN_PEER_MNANNOUNCE = 809010;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
